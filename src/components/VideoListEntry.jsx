e3fac3c46157973dd3b9d01db7425768537b8f64
Marina Cerame <marina.cerame@gmail.com>
2016-12-02 18:46:15 -0600

    trying to commit

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,15 +1,21 @@
-var VideoListEntry = () => (
+var VideoListEntry = (prop) => {
+  console.log(prop);
+  var that = prop.video.snippet;
+
+  var output = (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      <img className="media-object" src={that.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{that.title}</div>
+      <div className="video-list-entry-detail">{that.description}</div>
     </div>
   </div>
 );
 
+  return output;
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
