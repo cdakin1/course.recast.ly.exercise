827ba590b5ba27fa3580b670307ccf144e8febe9
Marina Cerame <marina.cerame@gmail.com>
2016-12-03 12:32:49 -0600

    Youtube Search Tests Pass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.videos[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideo: null,
+      videos: null
+    };
+
+    this.data = window.searchYouTube({key: window.YOUTUBE_API_KEY, query: 'cats', max: 5}, function(data) {
+      console.log('data', data);
+      return data;
+    });
+    console.log(this);
+  }
+
+  render() {
+    return (<div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.currentVideo}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={this.state.videos}/>
+      </div>
+    </div>);
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  }).done(data => {
+    console.log(data.items);
+    callback(data.items);
+  });
 };
 
 window.searchYouTube = searchYouTube;
